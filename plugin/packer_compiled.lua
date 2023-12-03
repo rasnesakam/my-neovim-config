85f5808406fb87c54a827514b84aedba5b0d9016
Ensar Makas <ensar.makas@gmail.com>
2023-12-03 10:09:43 +0300

    bu ne bilmiom compiled falan diyo

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -94,6 +94,11 @@ _G.packer_plugins = {
     path = "/home/master/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
     url = "https://github.com/VonHeikemen/lsp-zero.nvim"
   },
+  ["mason-lspconfig.nvim"] = {
+    loaded = true,
+    path = "/home/master/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    url = "https://github.com/williamboman/mason-lspconfig.nvim"
+  },
   ["mason.nvim"] = {
     loaded = true,
     path = "/home/master/.local/share/nvim/site/pack/packer/start/mason.nvim",
