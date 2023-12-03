53eaeb5cd557b9479e86d560a579832c1246bf98
Ensar Makas <ensar.makas@gmail.com>
2023-12-03 10:09:14 +0300

    Added mason as dependency

--- lua/emakas/packer.lua
+++ lua/emakas/packer.lua
@@ -22,14 +22,13 @@ return require('packer').startup(function(use)
 	use { 'theprimeagen/harpoon' }
 	use { 'mbbill/undotree' }
 	use { 'tpope/vim-fugitive' }
-	use { 'williamboman/mason.nvim' }
 	use {
 		'VonHeikemen/lsp-zero.nvim',
 		branch = 'v3.x',
 		requires = {
 			--- Uncomment these if you want to manage LSP servers from neovim
-			-- {'williamboman/mason.nvim'},
-			-- {'williamboman/mason-lspconfig.nvim'},
+			{'williamboman/mason.nvim'},
+			{'williamboman/mason-lspconfig.nvim'},
 
 			-- LSP Support
 			{'neovim/nvim-lspconfig'},
